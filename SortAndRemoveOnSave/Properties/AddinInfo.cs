86328a07a4ba6e91d743243651c8b02ab23541fd
Alex Sorokoletov <alex.sorokoletov@gmail.com>
2017-09-11 01:41:06 -0400

    Migrate from Xamarin Studio to Visual Studio for Mac

--- SortAndRemoveOnSave/Properties/AddinInfo.cs
+++ SortAndRemoveOnSave/Properties/AddinInfo.cs
@@ -5,10 +5,10 @@ using Mono.Addins.Description;
 [assembly: Addin(
     "SortAndRemoveOnSave",
     Namespace = "SortAndRemoveOnSave",
-    Version = "1.0"
+    Version = "1.1"
 )]
 
 [assembly: AddinName("SortAndRemoveOnSave")]
 [assembly: AddinCategory("IDE extensions")]
-[assembly: AddinDescription("Xamarin Studio Add-in to sort and remove usings when saving C# file")]
+[assembly: AddinDescription("Visual Studio for Mac Add-in to sort and remove usings when saving C# file")]
 [assembly: AddinAuthor("Alex Sorokoletov")]
